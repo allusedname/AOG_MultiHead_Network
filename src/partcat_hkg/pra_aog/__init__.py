d2dc18e523cbaefd488cb007d8ff7df65f3e85a5
Dongfeng Li <53164050+allusedname@users.noreply.github.com>
2026-06-23 18:28:47 -0400

    Add PRA-AOG package exports

---
+++ src/partcat_hkg/pra_aog/__init__.py
@@ -0,0 +1,50 @@
+from .bundle import (
+    PRAAOGBuildConfig,
+    PRAAOGBundle,
+    build_pra_aog_from_cache,
+    build_pra_aog_from_records,
+    load_pra_aog,
+    save_pra_aog,
+)
+from .motifs import (
+    MotifPursuitConfig,
+    SharedMotif,
+    SharedMotifBank,
+    compress_grammar_relations,
+)
+from .parser import PRAAOGConfig, PRAAOGParser, normalized_parse_scores
+from .readouts import posterior_readouts
+from .topdown import TopDownVerifier, TopDownVerifierConfig
+from .types import (
+    EdgeParse,
+    ParseForest,
+    ParseHypothesis,
+    SlotParse,
+    TopDownQuery,
+    VisibilityState,
+)
+
+__all__ = [
+    "PRAAOGBuildConfig",
+    "PRAAOGBundle",
+    "PRAAOGConfig",
+    "PRAAOGParser",
+    "MotifPursuitConfig",
+    "SharedMotif",
+    "SharedMotifBank",
+    "TopDownVerifier",
+    "TopDownVerifierConfig",
+    "VisibilityState",
+    "SlotParse",
+    "EdgeParse",
+    "ParseHypothesis",
+    "ParseForest",
+    "TopDownQuery",
+    "build_pra_aog_from_cache",
+    "build_pra_aog_from_records",
+    "compress_grammar_relations",
+    "load_pra_aog",
+    "normalized_parse_scores",
+    "posterior_readouts",
+    "save_pra_aog",
+]
