244206ecae48cd4ea80db50aabaa05f2c72557c1
Dongfeng Li <53164050+allusedname@users.noreply.github.com>
2026-07-04 23:45:21 -0400

    Export full ABG-HKG-AOG v7 components

--- src/partcat_hkg/abg_aog/__init__.py
+++ src/partcat_hkg/abg_aog/__init__.py
@@ -3,6 +3,17 @@ from .topdown import V7TopDownConfig, V7TopDownRenderer
 from .queryable_stage1 import CachedRequeryConfig, CachedRequeryer
 from .parser import ABGHKGAOGParser
 from .scene import SceneOwnershipConfig, GreedySceneAOGParser
+from .ports import PortConfig, ports_from_geom, port_pair_score
+from .structure_learning import (
+    BlockPursuitBank,
+    BlockPursuitConfig,
+    BlockPrototype,
+    EMBlockPursuitLearner,
+    SemiSupervisedExpander,
+    SemiSupervisedExpansionConfig,
+    response_from_terminal_records,
+)
+from .full import FullABGHKGAOGParser, FullV7Config, NativePartORLayer
 
 __all__ = [
     "V7AOGConfig",
@@ -17,4 +28,17 @@ __all__ = [
     "ABGHKGAOGParser",
     "SceneOwnershipConfig",
     "GreedySceneAOGParser",
+    "PortConfig",
+    "ports_from_geom",
+    "port_pair_score",
+    "BlockPursuitBank",
+    "BlockPursuitConfig",
+    "BlockPrototype",
+    "EMBlockPursuitLearner",
+    "SemiSupervisedExpander",
+    "SemiSupervisedExpansionConfig",
+    "response_from_terminal_records",
+    "FullABGHKGAOGParser",
+    "FullV7Config",
+    "NativePartORLayer",
 ]
